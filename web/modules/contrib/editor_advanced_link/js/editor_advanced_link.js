59c6e34cce3e41cc774090a4e839882166c9ecf2
Paul Foos <paul.foos@umb.edu>
2023-09-29 11:29:28 -0400

    D10 update for advanced link module

--- web/modules/contrib/editor_advanced_link/js/editor_advanced_link.js
+++ web/modules/contrib/editor_advanced_link/js/editor_advanced_link.js
@@ -1,37 +1,64 @@
-
-(function (Drupal, $) {
-
-  'use strict';
-
-  Drupal.behaviors.editor_advanced_link = {
-    attach: function (context, settings) {
-      // Reset modal window position when advanced details element is opened or
-      // closed to prevent the element content to be out of the screen.
-      $('.editor-link-dialog details[data-drupal-selector="edit-advanced"]').once('editor_advanced_link')
-        .on('toggle', function () {
-          $("#drupal-modal").dialog({
-            position: {
-              of: window
-            }
-          });
-      });
-      //Add noopener to rel attribute if open link in new window checkbox is checked
-      if($('input[data-drupal-selector="edit-attributes-rel"]').length) {
-        $('input[data-drupal-selector="edit-attributes-target"]')
-        .once('editor_advanced_linktargetrel')
-        .change(function () {
-          var rel_attribute_field = $('input[data-drupal-selector="edit-attributes-rel"]');
-          var rel_attributes = ' ' + 'noopener';
-          if (this.checked) {
-            rel_attribute_field.val(rel_attribute_field.val() + rel_attributes);
-            Drupal.announce(Drupal.t('the noopener attribute has been added to rel'));
-          } else {
-            rel_attribute_field.val(rel_attribute_field.val().replace(rel_attributes, ''));
-            Drupal.announce(Drupal.t('the noopener attribute has been removed from rel'));
-          }
-        });
-      }
-    }
+((Drupal, once, $) => {
+  const toggleDetails = () => {
+    $('#drupal-modal').dialog({
+      position: {
+        of: window,
+      },
+    });
   };
 
-}(Drupal, jQuery));
+  const targetCheckboxChange = (evt) => {
+    const checkbox = evt.currentTarget;
+    const relAttributeField = document.querySelector(
+      'input[data-drupal-selector="edit-attributes-rel"]',
+    );
+
+    let relAttributes = relAttributeField.value.split(' ');
+    if (checkbox.checked) {
+      relAttributes.push('noopener');
+      Drupal.announce(
+        Drupal.t('The noopener attribute has been added to rel.'),
+      );
+    } else {
+      relAttributes = relAttributes.filter((value) => value !== 'noopener');
+      Drupal.announce(
+        Drupal.t('The noopener attribute has been removed from rel.'),
+      );
+    }
+
+    // Remove empty items.
+    relAttributes = relAttributes.filter((value) => value.length);
+    // Deduplicate items.
+    relAttributes = [...new Set(relAttributes)];
+
+    relAttributeField.value = relAttributes.join(' ');
+  };
+
+  Drupal.behaviors.editor_advanced_link = {
+    attach(context) {
+      // Reset modal window position when advanced details element is opened or
+      // closed to prevent the element content to be out of the screen.
+      once(
+        'editor_advanced_link',
+        '.editor-link-dialog details[data-drupal-selector="edit-advanced"]',
+      ).forEach((details) => {
+        details.addEventListener('toggle', toggleDetails);
+      });
+
+      // Add noopener to rel attribute if open link in new window checkbox is
+      // checked.
+      if (
+        context.querySelector(
+          'input[data-drupal-selector="edit-attributes-rel"]',
+        )
+      ) {
+        once(
+          'editor_advanced_linktargetrel',
+          'input[data-drupal-selector="edit-attributes-target"]',
+        ).forEach((element) => {
+          element.addEventListener('change', targetCheckboxChange);
+        });
+      }
+    },
+  };
+})(Drupal, once, jQuery);
