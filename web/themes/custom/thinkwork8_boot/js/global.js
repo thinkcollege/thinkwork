4c9f6d3493b1e83d2fa188c3eb8fc84fb2a3ade5
Paul Foos <paul.foos@umb.edu>
2024-01-17 15:43:27 -0500

    js to make top menu more accessible, click not hover to open

--- web/themes/custom/thinkwork8_boot/js/global.js
+++ web/themes/custom/thinkwork8_boot/js/global.js
@@ -9,29 +9,41 @@
 
   Drupal.behaviors.thinkwork8_boot = {
     attach: function (context, settings) {
-
-    }
-  };
-})(jQuery, Drupal);
-
-(function($) {
-    $(function() {
-      $(document).tooltip({ selector: '[data-toggle="tooltip"]' });
-      $(document).popover({ selector: '[data-toggle="popover"]' });
-    });
-  })(jQuery);
-
-(function ($, Drupal) {
-
-  'use strict';
-
-  Drupal.behaviors.thinkwork8_boot = {
-    attach: function (context, settings) {
+      $(function() {
+        $(document).tooltip({ selector: '[data-toggle="tooltip"]' });
+        $(document).popover({ selector: '[data-toggle="popover"]' });
+      });
       $('button.navbar-toggler').click(function() {
         if (!$('#superfish-main-accordion').hasClass('sf-expanded')) {
           $('#superfish-main-toggle').click().hide();
         }
       });
+      $('a.sf-depth-1',context).on('mouseover', function(event) {
+      return false;
+      });
+      $('a.menuparent.sf-depth-1').on('click', function(event) {
+      // Close any open dropdown menus.
+      $.each($(this), function(index, element) {
+      $(element).parent().parent().find('li.menuparent > ul').addClass('sf-hidden');
+      });
+      // Open the clicked dropdown menu.
+      $(this).next().removeClass('sf-hidden');
+      event.preventDefault();
+      });
+
+        $('a.sf-depth-2',context).on('mouseover', function(event) {
+        return false;
+        });
+        $('a.menuparent.sf-depth-2').on('click', function(event) {
+        // Close any open dropdown menus.
+        $.each($(this), function(index, element) {
+        $(element).parent().parent().find('li.menuparent > ul').addClass('sf-hidden');
+        });
+        // Open the clicked dropdown menu.
+        $(this).next().removeClass('sf-hidden');
+        event.preventDefault();
+        });
+
     }
   };
 })(jQuery, Drupal);
