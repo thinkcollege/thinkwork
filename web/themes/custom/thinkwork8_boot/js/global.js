9f14c523e9ff3d2206132ce4527e833a6f271044
Paul Foos <paul.foos@umb.edu>
2024-04-18 11:43:43 -0400

    theme css, js, and templates for new chart builder

--- web/themes/custom/thinkwork8_boot/js/global.js
+++ web/themes/custom/thinkwork8_boot/js/global.js
@@ -22,52 +22,54 @@
         }
       });
       $('a.sf-depth-1',context).on('mouseover', function(event) {
-      return false;
+        return false;
       });
       $('a.menuparent.sf-depth-1').on('click', function(event) {
-      // Close any open dropdown menus.
-      $.each($(this), function(index, element) {
-      $(element).parent().parent().find('li.menuparent > ul').addClass('sf-hidden');
-      });
-      // Open the clicked dropdown menu.
-      $(this).next().removeClass('sf-hidden');
-      event.preventDefault();
-      });
-
-        $('a.sf-depth-2',context).on('mouseover', function(event) {
-        return false;
-        });
-        $('a.menuparent.sf-depth-2').on('click', function(event) {
         // Close any open dropdown menus.
-        $.each($(this), function(index, element) {
-        $(element).parent().parent().find('li.menuparent > ul').addClass('sf-hidden');
+       $.each($(this), function(index, element) {
+          $(element).parent().parent().find('li.menuparent > ul').addClass('sf-hidden');
         });
         // Open the clicked dropdown menu.
         $(this).next().removeClass('sf-hidden');
         event.preventDefault();
+      });
+
+        $('a.sf-depth-2',context).on('mouseover', function(event) {
+          return false;
         });
-        var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-        var eventer = window[eventMethod];
-        var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-
-        eventer(messageEvent,function(e) {
-
-          // If the message is a resize frame request
-          if (e.data.indexOf('reheight::') != -1) {
-            var height = e.data.replace('reheight::', '');
-
-            document.getElementById('chartBuilder').style.height = height + 'px';
-          }
-          else if (e.data.indexOf('rewide::') != -1) {
-            var width = e.data.replace('rewide::', '');
-            document.getElementById('chartBuilder').style.width = width + 'px';
-          }
-        } ,false);
-        $('input#bbState').on('click', function() {
-           var stateSelect = $('#selectState option').filter(':selected').val();
-           downloadState(stateSelect);
-
+        $('a.menuparent.sf-depth-2').on('click', function(event) {
+          // Close any open dropdown menus.
+          $.each($(this), function(index, element) {
+          $(element).parent().parent().find('li.menuparent > ul').addClass('sf-hidden');
         });
+        // Open the clicked dropdown menu.
+        $(this).next().removeClass('sf-hidden');
+          event.preventDefault();
+        });
+        once('chartBuilder',"#chartBuilder").forEach(function(value,i) {
+          var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+          var eventer = window[eventMethod];
+          var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+          eventer(messageEvent,function(e) {
+
+            // If the message is a resize frame request
+            if (e.data.indexOf('reheight::') != -1) {
+              var height = e.data.replace('reheight::', '');
+
+              document.getElementById('chartBuilder').style.height = height + 'px';
+            }
+            else if (e.data.indexOf('rewide::') != -1) {
+              var width = e.data.replace('rewide::', '');
+              document.getElementById('chartBuilder').style.width = width + 'px';
+            }
+          } ,false);
+          $('input#bbState').on('click', function() {
+            var stateSelect = $('#selectState option').filter(':selected').val();
+            downloadState(stateSelect);
+
+          });
+      });
         function downloadState(d) {
           window.location =  '/sites/default/files/bbstates/' + d + '.pdf';
         }
